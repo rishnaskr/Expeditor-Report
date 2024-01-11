539d64b10bc573f74bfcd2c574627949d0f02866
rishnaskr <91236217+rishnaskr@users.noreply.github.com>
2024-01-11 13:44:46 +0000

    login set

--- config/koneksi.php
+++ config/koneksi.php
@@ -3,8 +3,8 @@ $server = "151.106.97.204";
 $user = "u349600776_reeshdev";
 $pass = "Hostinger123!";
 $database = "u349600776_reeshdev";
-$conn = mysqli_connect($server, $user, $pass, $database);
-if (!$conn) {
+$kon = mysqli_connect($server, $user, $pass, $database);
+if (!$kon) {
     die("Koneksi ke database gagal: " . mysqli_connect_error());
 }
 ?>
