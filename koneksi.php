7b25103994d565f1ad5a6f5ea5a68aa4be8e6286
rishnaskr <91236217+rishnaskr@users.noreply.github.com>
2024-01-11 08:29:44 +0000

    menambahkan koneksi

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+$koneksi = mysqli_connect("u349600776_reeshdevhost","Hostinger123!","","u349600776_reeshdev");
+ 
+// Check connection
+if (mysqli_connect_errno()){
+	echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+ 
+?>
