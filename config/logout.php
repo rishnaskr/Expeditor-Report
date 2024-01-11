b3e9725a9e3c8410c7840c8337dac0c66ecdc7df
rishnaskr <91236217+rishnaskr@users.noreply.github.com>
2024-01-11 14:23:53 +0000

    logout

--- config/logout.php
+++ config/logout.php
@@ -1,21 +1,6 @@
-<?php
-// Mulai sesi (pastikan ini ditempatkan di awal file)
-session_start();
-
-// Cek apakah pengguna sudah login
-if(isset($_SESSION['username'])) {
-    // Hapus semua variabel sesi
-    session_unset();
-
-    // Hancurkan sesi
-    session_destroy();
-
-    // Redirect atau lakukan tindakan lain setelah logout
-    header("Location: ../login.php");
-    exit();
-} else {
-    // Pengguna tidak login, lakukan sesuatu (mungkin mengarahkan ke halaman login)
-    header("Location: ../login.php");
-    exit();
-}
+<?php   
+session_start(); //to ensure you are using same session
+session_destroy(); //destroy the session
+header("location:/index.php"); //to redirect back to "index.php" after logging out
+exit();
 ?>
