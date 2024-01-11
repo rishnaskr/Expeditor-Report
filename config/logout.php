cbff91365b96496c04cb5f5b4eecefddf3dd4327
riskimaulanarahman <61986465+riskimaulanarahman@users.noreply.github.com>
2024-01-11 14:53:03 +0000

    logout fix

--- config/logout.php
+++ config/logout.php
@@ -1,5 +1,12 @@
 <?php   
 session_start(); //to ensure you are using same session
-var_dump($_SESSION);
+
+$_SESSION['username']='';
+
+unset($_SESSION['username']);
+
+session_unset();
+session_destroy();
+header('Location:login.php');
 
 ?>
--- index.php
+++ index.php
@@ -743,7 +743,7 @@ $nama=$_SESSION["name"];
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.php">Logout</a>
+                    <a class="btn btn-primary" href="config/logout.php">Logout</a>
                 </div>
             </div>
         </div>
