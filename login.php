792223c104d2d9bc6faaddae14d3e78901408509
rishnaskr <91236217+rishnaskr@users.noreply.github.com>
2024-01-11 12:58:06 +0000

    set

--- login.php
+++ login.php
@@ -1,48 +1,4 @@
-<?php
-//menyertakan file program koneksi.php pada register
-require('config/koneksi.php');
-//inisialisasi session
-session_start();
-$error = '';
-$validate = '';
-//mengecek apakah sesssion username tersedia atau tidak jika tersedia maka akan diredirect ke halaman index
-if( isset($_SESSION['username']) ) header('Location: index.php');
-//mengecek apakah form disubmit atau tidak
-if( isset($_POST['submit']) ){
-        
-        // menghilangkan backshlases
-        $username = stripslashes($_POST['username']);
-        //cara sederhana mengamankan dari sql injection
-        $username = mysqli_real_escape_string($con, $username);
-         // menghilangkan backshlases
-        $password = stripslashes($_POST['password']);
-         //cara sederhana mengamankan dari sql injection
-        $password = mysqli_real_escape_string($con, $password);
-       
-        //cek apakah nilai yang diinputkan pada form ada yang kosong atau tidak
-        if(!empty(trim($username)) && !empty(trim($password))){
-            //select data berdasarkan username dari database
-            $query      = "SELECT * FROM users WHERE username = '$username'";
-            $result     = mysqli_query($con, $query);
-            $rows       = mysqli_num_rows($result);
-            if ($rows != 0) {
-                $hash   = mysqli_fetch_assoc($result)['password'];
-                if(password_verify($password, $hash)){
-                    $_SESSION['username'] = $username;
-               
-                    header('Location: index.php');
-                }
-                            
-            //jika gagal maka akan menampilkan pesan error
-            } else {
-                $error =  'Login Gagal !!';
-            }
-            
-        }else {
-            $error =  'Data tidak boleh kosong !!';
-        }
-    } 
-?>
+
 
 <!DOCTYPE html>
 <html lang="en">
